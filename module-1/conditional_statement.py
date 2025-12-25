f65e7c5f06246e05bff03e57ed9d6f3a47abf976
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-12-25 21:29:36 +0600

    conditional statement

---
+++ module-1/conditional_statement.py
@@ -0,0 +1,16 @@
+a = 7
+#a = a + 5
+# a +=5
+
+if a > 5:
+    print('5 er besi')
+    print('koto besi ke jane')
+elif a > 3:
+    print('olpo boro')
+elif a == 2:
+    print('ekdom dui er soman soman')
+else:
+    print('choto choto rate lombi hoye')
+
+
+
