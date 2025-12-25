6571fdc1216c33486d70940a9e3b6c7960be860b
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-12-25 21:40:48 +0600

    nested

---
+++ module-1/nested_cond_statement.py
@@ -0,0 +1,17 @@
+#nested conditonal statement 
+head = 'head'
+coin = head
+boss = False
+if boss ==  True:
+    print('boss you are joss')
+    if coin == 'tail':
+        print('batting')
+    else:
+        print('bowling')
+        if 5 > 2  or  boss != True:
+            print('do something')
+            if 8%2 == 0 and 5%2==1:
+                print('even 8 is an even number ')
+else:
+    print('you are not boss')
+
