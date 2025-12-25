b168e118d2e16d5f7055fb98da9381b1623670d8
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-12-25 21:06:19 +0600

    input & type caste

---
+++ input.py
@@ -0,0 +1,12 @@
+money = input('Give me some money:')
+print(money)
+
+first_money = input('Give me kalam some money:')
+second_money = input('Give me salim some money:')
+print(first_money,second_money)
+
+
+#by default the input from user will be string type
+first_money_int = int(first_money)
+print(type(first_money_int))
+print(type(first_money))
