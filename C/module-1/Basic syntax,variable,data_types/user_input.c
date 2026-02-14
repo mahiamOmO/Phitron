5d69afdf47b1d59078c5c30be1410e842f4eeb07
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2026-02-14 13:35:48 +0600

    user input

---
+++ C/module-1/Basic syntax,variable,data_types/user_input.c
@@ -0,0 +1,9 @@
+// user input scanf use korte hbe
+
+#include<stdio.h>
+int main() {
+    int rahim = 100, karim = 200;
+    scanf("%d %d",&rahim,&karim);
+    printf("%d %d",rahim,karim);
+    return 0;
+}
