2440717847d92846141067113fafc7956df3ee5f
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2026-02-15 16:42:20 +0600

    if-else

---
+++ C/module-2/operators,conditional statements/if-else_conditional.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+  int tk;
+  scanf("%d",&tk);
+  //if(tk == 100){
+  if(tk >= 100){
+    printf("Burger khabo");
+  }
+  else {
+    printf("saradin kichu khabo na");
+  }
+
+  return 0 ;
+}
